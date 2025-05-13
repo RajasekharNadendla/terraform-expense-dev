f03594141260e3499ead725ae3101094fbf02b1f
Rajasekhar <rajasekhar15052003@gmail.com>
2025-05-13 23:29:56 +0530

    dev terraform expense

--- 05-apps/expense.sh
+++ 05-apps/expense.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 dnf install ansible -y
 cd /tmp
-git clone https://github.com/RajasekharNadendla/Ansible_Roles.git 
-cd Ansible_Roles
-ansible-playbook -i inventory.ini main.yaml -e component=backend -e login_password=ExpenseApp1
-ansible-playbook -i inventory.ini main.yaml -e component=frontend
+git clone https://github.com/RajasekharNadendla/Ansible_Expense.git 
+cd Ansible_Expense
+ansible-playbook -i inventory.ini backend.yaml
+ansible-playbook -i inventory.ini frontend.yaml
 
