e8e1d886dd01548c7ed34137d3181c5898cc4aa2
Rajasekhar <rajasekhar15052003@gmail.com>
2025-05-14 10:12:47 +0530

    updated expense.sh file

--- 05-apps/expense.sh
+++ 05-apps/expense.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 dnf install ansible -y
 cd /tmp
-git clone https://github.com/RajasekharNadendla/Ansible_Expense.git 
-cd Ansible_Expense
+git clone https://github.com/RajasekharNadendla/Ansible_Roles.git
+cd Ansible_Roles
 ansible-playbook -i inventory.ini backend.yaml
 ansible-playbook -i inventory.ini frontend.yaml
+
+#git clone https://github.com/RajasekharNadendla/Ansible_Expense.git 
+#cd Ansible_Expense
+# ansible-playbook -i inventory.ini backend.yaml
+# ansible-playbook -i inventory.ini frontend.yaml
 
